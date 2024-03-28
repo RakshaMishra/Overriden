3c0881582ee4929efe1f49cbc80c02c630f08c37
Dell <Dell@DESKTOP-GIG0893>
2024-03-28 18:14:53 +0530

    overriden

---
+++ src/com/kodnest/overriden/Child.java
@@ -0,0 +1,22 @@
+package com.kodnest.overriden;
+
+public class Child extends Parent {
+	int a;
+	public void display()
+	{
+		System.out.println("its a child");
+	}
+	public void read()
+	{
+		System.out.println("reading");
+	}
+	void dance()
+	{
+		System.out.println("dancing");
+	}
+	void eat()
+	{
+		
+	}
+
+}
---
+++ src/com/kodnest/overriden/Parent.java
@@ -0,0 +1,5 @@
+package com.kodnest.overriden;
+
+public class Parent {
+
+}
